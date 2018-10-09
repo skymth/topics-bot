42e9db20108af23feb72f6bb5ff31fbfea5498df
soeyusuke <s1240231@gmail.com>
2018-10-09 21:48:41 +0900

    [ADD] get topics, attachment

--- handler.go
+++ handler.go
@@ -6,14 +6,28 @@ import (
 	"strings"
 
 	"github.com/nlopes/slack"
+	"github.com/pkg/errors"
+	"github.com/skymth/topics-bot/topics"
 )
 
-func setParams() slack.PostMessageParameters {
+func setParams(topic topics.Topic) slack.PostMessageParameters {
 	return slack.PostMessageParameters{
 		Attachments: []slack.Attachment{
-			slack.Attachment{},
+			slack.Attachment{
+				Color:     "#5df91b",
+				Title:     topic.Title,
+				TitleLink: topic.URL,
+				Text:      topic.Description,
+			},
 		},
 	}
+}
+
+func (s *Slack) send(event, label string, params slack.PostMessageParameters) error {
+	if _, _, err := s.client.PostMessage(event, label, params); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *Slack) TopicsPostHandler(event *slack.MessageEvent) error {
@@ -29,16 +43,21 @@ func (s *Slack) TopicsPostHandler(event *slack.MessageEvent) error {
 	}
 
 	m := strings.Split(strings.TrimSpace(event.Msg.Text), " ")[1:]
-	var label string
+
 	switch m[0] {
 	case "todays":
-		label = "hello!!!"
+		topics, err := topics.GetTopics()
+		if err != nil {
+			return errors.Wrap(err, "get topics err")
+		}
+		for _, topic := range topics {
+			if err := s.send(event.Channel, "", setParams(topic)); err != nil {
+				return errors.Wrap(err, "send topics err")
+			}
+		}
+
 	default:
 		return nil
-	}
-
-	if _, _, err := s.client.PostMessage(event.Channel, label, setParams()); err != nil {
-		return fmt.Errorf("[ERROR] post message failed: %s", err)
 	}
 
 	return nil
